a6ceff056241e6b22856a032cb596c278284154d
hbadshahph@gmail.com <hbadshahph@gmail.com>
2024-01-25 14:17:52 +0530

    RPI-sub_Micro_RFID

--- Manage data/excel_header.py
+++ Manage data/excel_header.py
@@ -1,11 +1,23 @@
 
 from openpyxl import load_workbook
+# Loading Workbook in read mode
 wb = load_workbook(filename='Book.xlsx',read_only=True)
+# getting work
 ws = wb['Sheet1']
+#for row in ws.rows:
+  #print(row)
+for x in range(1,5):
+  if (ws.cell(row=x,column=1).value)=='0x53ae22ad':
+    print(ws.cell(row=x,column=2).value)
+    print(ws.cell(row=x,column=3).value)
+    print(ws.cell(row=x,column=4).value)
+  #print(type(ws.cell(row=x,column=1).value))
 
-for row in ws.rows:
-  for cell in row:
-    print(cell.value)
+
+
+#for row in ws.rows:
+  #for cell in row:
+    #print(cell.value)
 
 # close the workbook after reading
 wb.close()
